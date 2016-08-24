4ada281e1669af655f2f713f0b9adfe3fb5b6be8
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-24 15:27:04 -0400

    Initial Commit. Add preliminary questions to vampire.rb (release 1)

---
+++ ruby/vampires.rb
@@ -0,0 +1,6 @@
+# 4.4 Jacob Williams
+
+puts "What is your name?"
+puts "How old are you? Where were you born?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Would you like to enroll in the company's health insurance?"
