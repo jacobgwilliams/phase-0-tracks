48d5cf47f59ca03f0c5356a064496bbc27f3c27f
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-24 16:40:34 -0400

    Add allergy checker. An allergy of 'sunshine' exits survey and indicates vampireness.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,43 +7,57 @@ survey_count = 0
 puts "How many employees are we interviewing today?"
 employee_count = gets.chomp.to_i
 while survey_count < employee_count
-  puts "What is your name?"
-  name = gets.chomp
-  puts "How old are you?"
-  age = gets.chomp.to_i
-  puts "What year were you born?"
-  birth_year = gets.chomp.to_i
-  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-  garlic_pref = gets.chomp
-  puts "Would you like to enroll in the company's health insurance? (y/n)"
-  insurance = gets.chomp
-
-  puts "Hello #{name}, here are the results of your vampire survey:"
-
-  # VAMPIRE SCORE CALCULATOR:
   vampire_score = 0
-  # Age:
-  if (age + birth_year) != 2015
-    vampire_score += 1
-    puts "Age indicates 1 point"
+  puts "First, please list all allergies one at a time, each followed by the ENTER key. When finished, type 'done'."
+  allergy = nil
+  entry = nil
+  until entry == "done"
+    entry = gets.chomp
+    allergy = entry
+    if allergy == "sunshine"
+      puts "Sunshine allergy alert!"
+      vampire_score += 4
+      break
+    end
   end
-  # Garlic Preference:
-  if garlic_pref == "n"
-    vampire_score += 1
-    puts "Garlic preference indicates 1 point"
-  end
-  # Insurance:
-  if insurance == "n"
-    vampire_score += 1
-    puts "Insurance preference indicates 1 point"
-  end
-  # Name:
-  if name == "Drake Cula"
-    vampire_score += 4
-    puts "Name indicates 4 points"
-  elsif name == "Tu Fang"
-    vampire_score += 4
-    puts "Name indicates 4 points"
+  if allergy != "sunshine"
+    puts "What is your name?"
+    name = gets.chomp
+    puts "How old are you?"
+    age = gets.chomp.to_i
+    puts "What year were you born?"
+    birth_year = gets.chomp.to_i
+    puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+    garlic_pref = gets.chomp
+    puts "Would you like to enroll in the company's health insurance? (y/n)"
+    insurance = gets.chomp
+
+    puts "Hello #{name}, here are the results of your vampire survey:"
+
+    # VAMPIRE SCORE CALCULATOR:
+    # Age:
+    if (age + birth_year) != 2015
+      vampire_score += 1
+      puts "Age indicates 1 point"
+    end
+    # Garlic Preference:
+    if garlic_pref == "n"
+      vampire_score += 1
+      puts "Garlic preference indicates 1 point"
+    end
+    # Insurance:
+    if insurance == "n"
+      vampire_score += 1
+      puts "Insurance preference indicates 1 point"
+    end
+    # Name:
+    if name == "Drake Cula"
+      vampire_score += 4
+      puts "Name indicates 4 points"
+    elsif name == "Tu Fang"
+      vampire_score += 4
+      puts "Name indicates 4 points"
+    end
   end
 
   # TALLYING SYSTEM:
