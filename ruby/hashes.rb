6c90cb9cdf2b13042a5a43350bad0bc8a926acf0
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-30 10:36:01 -0400

    Add initial user interface and prompt questions about client to be put into hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,36 @@
 # Get following details from client:
-# name, age, number of children, decor theme, etc. string, int, and bool data
+# name (string)
+# age (fixnum)
+# mobility challenges (bool)
+# number of children (fixnum)
+# decor theme (string)
+# etc. string, int, and bool data
 # All keys should be symbols unless a string is absolutely necessary
+
+client_details = {}
+
+puts "Client Information Form:"
+puts "What is the client's first and last name?"
+client_details[:name] = gets.chomp
+puts "What is the client's age?"
+client_details[:age] = gets.to_i
+puts "Are they mobility challenged? [y/n]"
+answer = gets.chomp
+if answer == "y"
+  client_details[:mobility_challenge] = TRUE
+elsif answer == "n"
+  client_details[:mobility_challenge] = FALSE
+else
+  client_details[:mobility_challenge] = nil
+end
+puts "How many children do they have?"
+client_details[:num_children] = gets.to_i
+puts "What kind of decor theme do they want?"
+client_details[:decor_theme] = gets.chomp
+
+puts "PROCESSING..."
+puts "Client detail sheet:"
+p client_details
 
 # Program should also:
 # Prompt designer/user for this info
