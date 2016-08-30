703e22ee9ae19968657b59d67640a04dc2b18790
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-30 15:21:33 -0400

    Recieve user input. Break down into an array of two names and swap first and last name. Split new string into character array, set preliminary .next method on each character.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,3 +14,22 @@
 # Handle edge cases ('z')
 # Deal with upper case letters, perhaps by making everything lower case first, then going up case on the first letter of each word in the joined string.
 
+# Get user name, set downcase, split, and reverse first and last name
+def get_name
+  puts "What is your first and last name? Please separate with a space."
+  name = gets.chomp.downcase.split(' ').reverse.join(' ')
+end
+reverse_name = get_name
+p reverse_name
+
+split_reverse = reverse_name.chars
+new_name = ""
+split_reverse.map! do |letter|
+  if letter == " "
+    new_name += " "
+  else
+    new_name += letter.next
+  end
+end
+p new_name
+# DRIVER CODE
