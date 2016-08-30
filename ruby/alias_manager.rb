2a8927a8e363c5a4a61d3bdb14ff85b5703a74f9
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-30 15:03:26 -0400

    Initial Commit. Create base algorithm for alias manager.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# 5.5 Solo Challenge
+# Jacob Williams
+
+# PSEUDOCODE:
+# Take a real name
+## Get input from user, request a first and last name.
+## Split into an array
+# Swap first and last name
+## Reverse contents of above array
+# Change each vowel to the next vowel in the sequence 'aeiou'
+## Iterate to cycle through every vowel and change it to next in sequence
+# Change all consonants to next consonant in alphabet 'bcdfghjklmnpqrstvwxyz'
+## Iterate to cycle throuh every consonant and change it to next in sequence
+# Handle edge cases ('z')
+# Deal with upper case letters, perhaps by making everything lower case first, then going up case on the first letter of each word in the joined string.
+
