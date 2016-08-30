08c8197948cf9eb611d425710b3d9cae74d9ca9f
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-30 00:56:50 -0400

    Initial commit. Begin pseudocoding hash program.

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+# Get following details from client:
+# name, age, number of children, decor theme, etc. string, int, and bool data
+# All keys should be symbols unless a string is absolutely necessary
+
+# Program should also:
+# Prompt designer/user for this info
+# Convert any user input into appropriate data type
+# Print the hash back to screen when designer answers all questions
+# Give user opp to update a key. Escape if user says "none." If user enters "decor_theme," program should ask for new key value and update key. (Strings have methods to turn them into symbols.)
+# Print latest varsion of hash, exit
