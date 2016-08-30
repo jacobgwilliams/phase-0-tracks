4d338da976d3e5f4bc066a430e609db2c6a83755
Jacob Williams <2jacobwilliams@gmail.com>
2016-08-30 13:44:41 -0400

    Release 2: Search for new methods in Ruby docs to play with arrays and hashes.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -31,3 +31,28 @@ fruits.map! do |fruit|
   fruit.upcase
 end
 p fruits
+
+# RELEASE 2
+
+numbers = [1, 2, 3, 4, 5, 2562345, 75, 434]
+
+# 1. delete_if
+
+p numbers.delete_if{|num| num%2 == 0 }
+
+# 2. find (filter for specific items)
+
+# p numbers.delete_if{|num| num > 10}
+
+p numbers.keep_if {|num| num == 3 }
+
+# 3. A different method that filters a data structure for only items satisfying a certain condition
+
+numbers = [1, 2, 3, 4, 5, 2562345, 75, 434]
+p numbers.find_all {|num| num%2 != 0}
+# numbers.select also works
+
+# 4. method that removes items from a structure until condition in the block is false
+
+numbers = [1, 2, 2562345, 3, 4, 5, 75, 434]
+p numbers.drop_while {|num| num < 6}
