605ab9ed812a8507088d383a99fe3b9d93a4b045
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-06 12:54:30 -0400

    Create Clown class. Give clown function and loop through it 50 times.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,6 @@
+# MPS 6.2
+# Arsy and Jacob
+
 class Puppy
   def initialize
     puts "Initializing new puppy instance..."
@@ -26,9 +29,38 @@ class Puppy
 
 end
 
-puppy = Puppy.new
-puppy.fetch("ball")
-puppy.speak(4)
-puppy.roll_over
-puppy.dog_years(5)
-puppy.play_dead
+# puppy = Puppy.new
+# puppy.fetch("ball")
+# puppy.speak(4)
+# puppy.roll_over
+# puppy.dog_years(5)
+# puppy.play_dead
+
+class Clown
+  def initialize
+    puts "Initializing new clown..."
+  end
+
+  def juggle(count, objects)
+    puts "I'm juggling #{count} #{objects}!"
+  end
+
+  def laughs(count)
+    count.times {puts "HA!"}
+  end
+
+  def slips(object)
+    puts "UH OH I SLIPPED ON A #{object}!"
+  end
+end
+
+
+clowns = []
+50.times { clown = Clown.new
+      clowns.push(clown)}
+
+clowns.each do |clown|
+  clown.juggle(5, "chainsaws")
+  clown.laughs(3)
+  clown.slips("banana peel")
+end
