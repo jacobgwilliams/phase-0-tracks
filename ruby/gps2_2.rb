1b8af3ee5b10e5310de03612eff00c0bb037e207
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-05 20:20:34 -0400

    Finish rest of methods for grocery list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -9,18 +9,16 @@
 # output: hash
 
 string = "carrots apples cereal pizza"
+@grocery_hash = {}
 
 def create_list(string)
-  grocery_hash = {}
   grocery_array = string.split
   grocery_array.each do |item|
-    grocery_hash[item] = 0
-  end
-  puts "Your grocery list:"
-  grocery_hash.each do |item, quant|
-    puts "- #{item}: #{quant}"
+    @grocery_hash[item] = 0
   end
 end
+
+create_list(string)
 
 # Method to add an item to a list
 # input: item name and optional quantity
@@ -28,18 +26,48 @@ end
 # split input and add to hash as a key and value pair
 # output: updated hash
 
+def add_item(item, quant=0)
+  @grocery_hash[item] = quant
+end
+
+add_item("dogfood", 2)
+p @grocery_hash
+
 # Method to remove an item from the list
 # input: item (key value)
 # steps: delete item and quanity from hash (.delete)
 # output: updated hash
+
+def remove_item(item)
+  @grocery_hash.delete(item)
+end
+
+remove_item("apples")
+p @grocery_hash
 
 # Method to update the quantity of an item
 # input: item name, quantity
 # steps: use item name to access hash key and input value to update value
 # output: updated hash
 
+def update_quantity(item, quant)
+  @grocery_hash[item] = quant
+end
+
+update_quantity("cereal", 5)
+p @grocery_hash
+
 # Method to print a list and make it look pretty
 # input: most updated hash
 # steps: use interpolation to print items in a list form as strings
 # (iterating through each key/value pair)
 # output: a fancy list
+
+def pretty_list
+  puts "Your grocery list:"
+  @grocery_hash.each do |item, quant|
+    puts "- #{item}: #{quant}"
+  end
+end
+
+pretty_list
