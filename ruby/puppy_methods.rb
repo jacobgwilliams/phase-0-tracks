d80a93ec35511a21a79fa6297c93e891ef381235
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-06 12:43:44 -0400

    Create Puppy class and add multiple methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,34 @@
 class Puppy
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(count)
+    count.times{p "Woof!"}
+  end
+
+  def roll_over
+    puts "**rolls over**"
+  end
+
+  def dog_years(human_years)
+    p "#{human_years*7}"
+  end
+
+  def play_dead
+    puts "**dies**"
+  end
+
 end
 
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(4)
+puppy.roll_over
+puppy.dog_years(5)
+puppy.play_dead
