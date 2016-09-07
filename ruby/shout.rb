7eb9281e04938724ec2527635dbeb3012c95778c
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-07 11:21:36 -0400

    Edit Flight module. Also refactor Shout to be a mixin for Grandma and Grandpa class.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,40 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     "OH MY " + words.upcase + " This is the happiest day of my life!"
+#   end
+# end
+
+# p Shout.yell_angrily("damn it")
+# p Shout.yelling_happily("cupcake sauce")
+
+### RELEASE 3 ###
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(name)
+    name + " GET OFF MY LAWN!!!"
   end
 
-  def self.yelling_happily(words)
-    "OH MY " + words.upcase + " This is the happiest day of my life!"
+  def yelling_happily(gameshow)
+    "OH BOY, " + gameshow.upcase + " IS ON!"
   end
 end
 
-p Shout.yell_angrily("damn it")
-p Shout.yelling_happily("cupcake sauce")
+class Grandma
+  include Shout
+end
+
+class Grandpa
+  include Shout
+end
+
+# DRIVER CODE
+
+eustace = Grandpa.new
+p eustace.yell_angrily("Allen")
+
+janice = Grandma.new
+p janice.yelling_happily("Wheel of Fortune")
