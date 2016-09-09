0a75083a1d69905d9997fd495a8187da025eeaf5
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-09 12:49:22 -0400

    Pass all 4 tests.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,12 +1,23 @@
+# 6.5 Mandatory Pairing
+
 class TodoList
   def initialize(input)
-    @items = ["do the dishes", "mow the lawn"]
+    @items = input
   end
 
   def get_items
     @items
   end
 
-  def add_item
+  def add_item(item)
+    @items << item
+  end
+
+  def delete_item(item)
+    @items.delete(item)
+  end
+
+  def get_item(ind)
+    @items[ind]
   end
 end
