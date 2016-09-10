e0b8f2a89a7e8a2e4c4f25d1f836f05081a2664e
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-10 01:18:58 -0400

    Set up new algorithm, laid out tests to run for WordGuess. Program stores answer.

--- ruby/word_guess/word_guess.rb
+++ ruby/word_guess/word_guess.rb
@@ -1,45 +1,49 @@
 # 6.6 Word Guesser
 # Jacob Williams
 
-# make a class for a word guessing game
-# one user can enter a word or phrase
-# For driver code, just input a default word
-# Another user attempts to guess the word
-# guesses should be limited (should depend on the length of the word)
-# break down word
-# Repeated guesses don't count
-# (store guesses)
-# (check guesses)
-# Guessing player recieves continual feedback ("_ _ _ _ _ _", if user guesses a correct letter, show it: "_ _ _ c _ _")
-# Congratulate user if they win, taunt them if they lose
+# PSEUDOCODE:
+# One user can enter a word, another user attempts to guess the word
+# Guesses are limited based on the length of the word
+# Repeated guesses do not count against the user
+# Guess player recieved continual feedback ("_ _ _ _ _ _")
+# Congratulatory message if win, taunt if lose
 
 class WordGuess
-  attr_reader :guess, :answer
-
-  def initialize(guess, answer)
-    @guess = guess
+  attr_reader :answer
+  def initialize(answer)
     @answer = answer
-    @hint = ''
-    @attempts = answer.length
-  end
-
-  def max_tries
-    puts "You have #{@attempts} attempts left."
-    @attempts
-  end
-
-  def initial_hint
-    answer.length.times do |underscore|
-      @hint += "_ "
-    end
-    @hint.strip!
-  end
-
-  def progress
-
-  end
-
-  def word_checker
-    answer_array = @answer.split
   end
 end
+
+######################################
+
+# class WordGuess
+#   attr_reader :guess, :answer
+
+#   def initialize(guess, answer)
+#     @guess = guess
+#     @answer = answer
+#     @hint = ''
+#     @attempts = answer.length
+#   end
+
+#   def max_tries
+#     puts "You have #{@attempts} attempts left."
+#     @attempts
+#   end
+
+#   def initial_hint
+#     answer.length.times do |underscore|
+#       @hint += "_ "
+#     end
+#     @hint.strip!
+#   end
+
+#   def progress
+
+#   end
+
+#   def word_checker
+#     answer_array = @answer.split
+#   end
+# end
--- ruby/word_guess/word_guess_spec.rb
+++ ruby/word_guess/word_guess_spec.rb
@@ -1,25 +1,49 @@
 require_relative 'word_guess'
 
 describe WordGuess do
-  let(:game) { WordGuess.new("banana", "banana") }
-
-  it "stores user guess on initialization" do
-    expect(game.guess).to eq "banana"
-  end
-
   it "stores answer on initialization" do
-    expect(game.answer).to eq "banana"
+    game = WordGuess.new("banana")
+    game.answer == "banana"
   end
 
-  it "bases max number of tries on length of word" do
-    expect(game.max_tries).to eq 6
+  it "limits guesses based on length of word" do
   end
 
-  it "shows hint as underscores for letters" do
-    expect(game.initial_hint).to eq "_ _ _ _ _ _"
+  it "does not count repeat guesses" do
   end
 
-  it "replaces underscores with correct letters when guessed" do
-    expect(game.progress).to eq ""
+  it "provides continual feedback" do
+  end
 
+  it "provides a congrats message on winning" do
+  end
+
+  it "provides a taunt if user loses" do
+  end
 end
+
+########################
+
+# describe WordGuess do
+#   let(:game) { WordGuess.new("banana", "banana") }
+
+#   it "stores user guess on initialization" do
+#     expect(game.guess).to eq "banana"
+#   end
+
+#   it "stores answer on initialization" do
+#     expect(game.answer).to eq "banana"
+#   end
+
+#   it "bases max number of tries on length of word" do
+#     expect(game.max_tries).to eq 6
+#   end
+
+#   it "shows hint as underscores for letters" do
+#     expect(game.initial_hint).to eq "_ _ _ _ _ _"
+#   end
+
+#   it "replaces underscores with correct letters when guessed" do
+#     expect(game.progress).to eq ""
+#   end
+# end
