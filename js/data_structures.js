e3e29521407794cebc4963ca675735cbfdb9001d
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-14 13:59:37 -0400

    Create an object constructor for Cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,25 @@ for (var i = 0; i < colors.length; i++) {
 }
 
 console.log(horses);
+
+// Car maker
+
+function Car(color, year, make, model, hasFourWheelDrive){
+  console.log("New car:", this);
+  this.color = color;
+  this.year = year;
+  this.make = make;
+  this.model = model;
+  this.hasFourWheelDrive = hasFourWheelDrive;
+
+  this.revEngine = function() { console.log("Vroom vroom!"); };
+
+  console.log("CAR COMPLETE");
+}
+
+var aCar = new Car("blue", 1999, "Ford", "Taurus", false);
+var alinasCar = new Car("white", 2001, "Honda", "Civic", false);
+console.log(aCar);
+console.log(alinasCar);
+aCar.revEngine();
+alinasCar.revEngine();
