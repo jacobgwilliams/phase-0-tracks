103a9a134755a23a702734b39efad24de6a76528
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-13 23:09:23 -0400

    Add reflection.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -90,3 +90,14 @@ end
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# One uses hash rockets with strings for keys, the other uses symbols with the colon initializer.
+# What does require_relative do? How is it different from require?
+# require_relative requires from a file based on the current location of the asking file. require is different, because it needs a full path to a required file, but also require is the only command that can require Ruby gems.
+# What are some ways to iterate through a hash?
+# We used .each with parameters for keys and hashes.
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# Due to "over-writing" or "over-coding," it wouldn't have occurred to me not to pass those instance variables into those methods. Because they are instance variables and are available anywhere in the Class, you don't even need to pass them in.
+# What concept did you most solidify in this challenge?
+# I think scope was a little more solidified on this challenge, as well as the idea of refactoring. I always err on the side of writing longer if/else chains, and I think it's important to always consider ways using arithmatic to shorten those cases.
