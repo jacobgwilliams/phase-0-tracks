1c49ad609d294f98f3db9b39e8b31723b198dd64
Jacob Williams <2jacobwilliams@gmail.com>
2016-09-28 20:00:24 -0400

    Add weird Cat DJ gif functionality

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,34 @@
 console.log("Testing! The script is running!");
+
+function giveCatBorder(event) {
+  console.log("giveCatBorder is running");
+  event.target.style.border = "3px solid blue";
+}
+
+var catImage = document.getElementById('cat');
+catImage.addEventListener("click", giveCatBorder);
+
+var els = document.getElementsByTagName('em');
+var el = els[0];
+el.addEventListener("mouseover", giveCatBorder);
+
+function makeDJCatAppear() {
+var djDiv = document.getElementById("DJ");
+if (djDiv.children.length == 1) {
+  console.log("makeDJCatAppear is running");
+  var DJCat = document.createElement('img');
+  DJCat.src = "DJ.gif";
+  DJCat.setAttribute("alt", "A DJ cat!");
+
+  // var djDiv = document.getElementById("DJ");
+  djDiv.appendChild(DJCat);
+  // djDiv.firstChild.removeEventListener("mouseover", makeDJCatAppear); // ONE DJ ONLY
+} else {
+  console.log("You already have a DJ cat");
+  djDiv.removeChild(djDiv.children[1]);
+}
+}
+
+var djDiv = document.getElementById("DJ");
+djDiv.firstChild.addEventListener("mouseover", makeDJCatAppear);
+// appendChild(DJCat);
